22995dee85edd727d965c9408c7303aee42dcfaf
AsaLap <asalaporte@gmail.com>
2020-04-03 18:00:23 +0200

    Dictionary working well, save and load next

--- blasting.py
+++ blasting.py
@@ -15,35 +15,29 @@ def get_genes_reactions(_path):
     Args :
         _path -- the reference GEM in SBML format.
     Return : 
-        genes_and_reactions -- a dictionary with the gene name as key and a dictionary
-        as value, containing the reactions and their formulas,
-        and the results of blastp.
+        gene_dic -- a dictionary with the gene name as key and two dictionaries as values,
+        one for the reactions (IDs and formula) and the other one for the blastp result.
     '''
-    genes_and_reactions = {}
+    gene_dic = {}
     model = cobra.io.read_sbml_model(_path)
-    for i in range(len(model.genes)):
-        gene = model.genes[i].name
-        try:
-            reac = model.reactions[i]
-        except IndexError:
-            print("Reaction not found : ",i)
-            reac = "NA"
-            pass
-        genes_and_reactions[gene] = {reac.id:[reac.reaction]}
-    return genes_and_reactions
+    for gene in model.genes:
+        gene_dic[gene.name]={'Reactions':{}}
+        for i in gene.reactions:
+            gene_dic[gene.name]['Reactions'][i.id] = i.reaction
+    return gene_dic
 
-def blast_p(dictionary, workDir, subjectFile, queryFile):
+def blast_p(gene_dic, workDir, subjectFile, queryFile):
     """Runs multiple blastp between a subject file and each protein of the query file.
     
     ARGS : 
-        dictionary -- the dictionary containing the genes and reactions,
+        gene_dic -- the dictionary containing the genes and reactions,
         which will gather the results of the blastp.
         workDir -- the working directory where the files are, and where the proteins files
         will be temporarily stored.
         subjectPATH -- the subject file for the blastp.
         queryPATH -- the query file for the blastp corresponding to the BiModel object given.
     RETURN : 
-        dictionary -- the input dictionary containing the result of the blastp command for each gene.
+        gene_dic -- the input dictionary containing the result of the blastp command for each gene.
     """
     newDir = workDir + "Proteins/"
     queryDir = workDir + queryFile
@@ -65,8 +59,8 @@ def blast_p(dictionary, workDir, subjectFile, queryFile):
     ###Blastp###
     print("\nLaunching the blastp !")
     i = 1
-    x = len(dictionary.keys())
-    for gene in dictionary.keys():
+    x = len(gene_dic.keys())
+    for gene in gene_dic.keys():
         if i%50==0:
             print("Protein %i out of %i" %(i,x))
         i+=1
@@ -78,9 +72,10 @@ def blast_p(dictionary, workDir, subjectFile, queryFile):
             newDir+"in_"+gene+".fa",
             "-outfmt",
             "10 delim=, qseqid qlen sseqid slen length nident pident score evalue bitscore"]
-        dictionary[gene]['BlastP'] = subprocess.run(requestBlastp, capture_output=True).stdout.decode('ascii').split("\n")[:-1]
-        print(dictionary[gene])
+        gene_dic[gene]['BlastP'] = subprocess.run(requestBlastp, capture_output=True).stdout.decode('ascii').split("\n")[:-1]
+        print(gene_dic[gene])
     subprocess.run(["rm", "-rf", newDir])
+    return gene_dic
 
 
 if __name__=='__main__':
