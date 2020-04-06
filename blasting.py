fdf545b9d8aa23d9f7d155e492af08558d117480
AsaLap <asalaporte@gmail.com>
2020-04-06 14:52:50 +0200

    Total blast done, plot on the percentage of coverage done

--- blasting.py
+++ blasting.py
@@ -8,6 +8,8 @@ import cobra
 from os.path import join
 import subprocess
 import pickle
+import time
+import matplotlib.pyplot as plt
 
 
 def get_genes_reactions(_path):
@@ -26,6 +28,7 @@ def get_genes_reactions(_path):
         for i in gene.reactions:
             gene_dic[gene.name]['Reactions'][i.id] = i.reaction
     return gene_dic
+
 
 def blast_p(gene_dic, workDir, subjectFile, queryFile):
     """Runs multiple blastp between a subject file and each protein of the query file.
@@ -46,6 +49,7 @@ def blast_p(gene_dic, workDir, subjectFile, queryFile):
     subprocess.run(["mkdir", newDir])
     
     ###Creation of one file per protein of the query###
+    start_time = time.time()
     print("\nCreating the small fasta files...")
     fileFasta = open(queryDir)
     queryFasta = fileFasta.read()
@@ -57,15 +61,18 @@ def blast_p(gene_dic, workDir, subjectFile, queryFile):
     fileFasta.close()
     print("...done !")
     
-    ###Blastp###
-    print("\nLaunching the blastp !")
+    ###Blast###
+    print("\nLaunching the blast !")
     i = 1
     x = len(gene_dic.keys())
+    total_time = time.time()
+    lap_time = time.time()
     for gene in gene_dic.keys():
-        if i%50==0:
-            print("Protein %i out of %i" %(i,x))
+        if i%10==0:
+            print("Protein %i out of %i\nTime : %f s" %(i,x, time.time() - lap_time))
+            lap_time = time.time()
         i+=1
-        requestBlastp = [
+        requestBlast = [
             "blastp",
             "-subject",
             subjectDir,
@@ -73,29 +80,67 @@ def blast_p(gene_dic, workDir, subjectFile, queryFile):
             newDir+"in_"+gene+".fa",
             "-outfmt",
             "10 delim=, qseqid qlen sseqid slen length nident pident score evalue bitscore"]
-        gene_dic[gene]['BlastP'] = subprocess.run(requestBlastp, capture_output=True).stdout.decode('ascii').split("\n")[:-1]
+        gene_dic[gene]['BlastP'] = subprocess.run(requestBlast, capture_output=True).stdout.decode('ascii').split("\n")[:-1]
     subprocess.run(["rm", "-rf", newDir])
+    print("Total time : %f s" %(time.time() - total_time))
     return gene_dic
+
 
 def save_obj(obj, path):
     with open(path + '.pkl', 'wb+') as f:
         pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
 
+
 def load_obj(path):
     with open(path + '.pkl', 'rb') as f:
         return pickle.load(f)
 
+
+def make_graph(dict_data):
+    res_plot = {}
+    treshold = 5
+    while treshold <= 100:
+        count = 0
+        for key in dict_data.keys():
+            for res in dict_data[key]['BlastP']:
+                if float(res.split(",")[6]) >= treshold:
+                    count+=1
+        res_plot[treshold] = count
+        treshold+=5
+    x = list(res_plot.keys())
+    y = list(res_plot.values())
+    return x, y
+
+
 if __name__=='__main__':
     ###Files and working directory###
-    WD = '/home/asa/INRAE/Work/Drafts/Data/Tests/'
+    WDtom = '/home/asa/INRAE/Work/Drafts/Data/Tomato_Arabidopsis/'
+    WDkiw = '/home/asa/INRAE/Work/Drafts/Data/Kiwi_Arabidopsis/'
+    WDche = '/home/asa/INRAE/Work/Drafts/Data/Cherry_Arabidopsis/'
+    WDcuc = '/home/asa/INRAE/Work/Drafts/Data/Cucumber_Arabidopsis/'
+    
+    # WD = '/home/asa/INRAE/Work/Drafts/Data/Tests/'
+    
     modelGem = 'AraGEM3.xml'
     modelGemFasta = 'genomic.in.fasta'
     modelGemFastaTest = 'testBlast.in.fasta'
     tomatoFasta = 'ITAG4.0_proteins.fasta'
     tomatoFastaTest = 'TomatoTest.fasta'
+    cherryFasta = 'PRUAV_Regina.fa'
+    cucumberFasta = 'Gy14_pep_v2.fa'
+    kiwiFasta = 'Hongyang_pep_v2.0.fa'
     
     ###Pipeline###
-    core_info = get_genes_reactions(WD+modelGem)
-    core_info = blast_p(core_info, WD, tomatoFastaTest, modelGemFasta)
-    save_obj(core_info, WD + "dictionary")
-    # test = load_obj(WD + "dictionary")
+    # core_info = get_genes_reactions(WD+modelGem)
+    # core_info = blast_p(core_info, WD, kiwiFasta, modelGemFasta)
+    # save_obj(core_info, WD + "dictionary")
+    tom = load_obj(WDtom + "dictionary")
+    kiw = load_obj(WDkiw + "dictionary")
+    che = load_obj(WDche + "dictionary")
+    cuc = load_obj(WDcuc + "dictionary")
+    tomato = make_graph(tom)
+    kiwi = make_graph(kiw)
+    cherry = make_graph(che)
+    cucumber = make_graph(cuc)
+    plt.plot(tomato[0], tomato[1], 'r', kiwi[0], kiwi[1], 'g', cucumber[0], cucumber[1], 'b', cherry[0], cherry[1], 'y')
+    plt.show()
