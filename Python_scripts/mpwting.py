7bfe555e2b84f74e54e1bcb37ed86ce795127aa3
AsaLap <asalaporte@gmail.com>
2021-11-10 22:57:29 +0100

    Small init file modification

--- Python_scripts/mpwting.py
+++ Python_scripts/mpwting.py
@@ -248,7 +248,7 @@ def create_mpwt_objects(main_directory, input_directory):
 
     taxon_name_list = []
 
-    parameters = utils.read_config(main_directory + "mpwting.ini")
+    parameters = utils.read_config(main_directory + "main.ini")
     cpu = len(parameters.keys()) - 1
     for i in parameters.keys():
         if i != "DEFAULT":
