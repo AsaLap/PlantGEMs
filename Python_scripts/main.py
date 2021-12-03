fd1f8c9e9f44a19ad2131cdfd913a6c09a96bb44
AsaLap <asalaporte@gmail.com>
2021-12-03 12:13:34 +0100

    Comments

--- Python_scripts/main.py
+++ Python_scripts/main.py
@@ -15,14 +15,14 @@ import sys
 
 
 def run(main_directory):
-    print("Proceding to create all the needed files and checking input files, please stay around...")
-    # Launching the first part of Blast (files checking & generation)
+    print("Proceeding to create all the needed files and checking input files, please stay around...")
+    # Launching the first part of Blast (files checking & folder generation)
     list_objects_to_blast = blasting.sub_pipeline_first(main_directory)
-    # Launching the first part of MPWT (files checking & generation)
+    # Launching the first part of MPWT (files checking & folder generation)
     list_objects, cpu, input_directory, output_directory, log_directory = mpwting.sub_pipeline_first(main_directory)
 
     # Then, launching the rest of the pipeline in multiprocess without the need of any input from the user
-    print("Everything's fine, now launching BLAST and MPWT processes, it may take some time...")
+    print("Everything's fine, now launching BLAST and then MPWT processes, it may take some time...")
     blasting.sub_pipeline_last(list_objects_to_blast)
     mpwting.sub_pipeline_last(list_objects, cpu, input_directory, output_directory, log_directory)
 
