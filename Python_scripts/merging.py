e872e1e3daba0c7be94bc4982dfa911721e90465
AsaLap <asalaporte@gmail.com>
2022-01-07 18:31:02 +0100

    Merging working, parameters left to configure and main to wire

--- Python_scripts/merging.py
+++ Python_scripts/merging.py
@@ -13,6 +13,8 @@ import copy
 import os
 import re
 
+from datetime import date
+
 import module
 import utils
 
@@ -29,7 +31,8 @@ class Merging(module.Module):
         self.pwt_metacyc_long_id_list = []
         self.json_reactions_list = []
         self.sbml_reactions_list = []
-
+        self.merged_model = cobra.Model(self.name, name=self.name + "_PlantGEMs_" + str(date.today()))
+        self.metacyc_model = cobra.io.load_json_model(self.main_directory + "merge/metacyc.json")
         self.metacyc_ids_file_path = utils.find_file(self.main_directory + "merge/", "metacyc_ids", "tsv")
         if not os.path.isfile(self.metacyc_ids_file_path):
             utils.get_metacyc_ids(self._find_sbml_model(self.directory))
@@ -148,7 +151,7 @@ class Merging(module.Module):
         reaction.gene_reaction_rule = " or ".join(set(gene_list))
         return reaction
 
-    def _merge(self, trust_model=False, verbose=False):
+    def _merge(self, trust_model=True, verbose=False):
         """Function to merge models, either from a model-based reconstruction (blasting module) or from
         Pathway Tools's Pathologic software.
 
@@ -159,38 +162,35 @@ class Merging(module.Module):
             verbose (boolean) -- print or not the protein matches in the terminal (won't change the logs).
         """
 
-    #     self._get_pwt_reactions()
-    #     self._get_blast_model_reactions()
-    #     # Here we take away the reactions of the blast reconstruction because we will deep-copy them as they are
-    #     # already correctly linked to the good genes and we just need to correct the gene reaction rule from the
-    #     # pathway tools reconstruction.
-    #     metacyc_reactions_set = set(self.pwt_reactions_list) - set.intersection(set(self.pwt_reactions_list),
-    #                                                                             set(self.blast_reactions_list))
-    #     new_model = copy.deepcopy(self.blast_model)
-    #     list_fail = []
-    #     for reaction in metacyc_reactions_set:
-    #         try:
-    #             if reaction[0] in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']:
-    #                 reaction = "_" + reaction
-    #             added_reactions = copy.deepcopy(self.metacyc_model.reactions.get_by_id(reaction))
-    #             added_reactions_corrected = self._correct_gene_reaction_rule(added_reactions, verbose)
-    #             new_model.add_reactions([added_reactions_corrected])
-    #         except KeyError:
-    #             list_fail.append(reaction)
-    #     print("Nb of reactions not found in the metacyc model: ", len(list_fail))
-    #     cobra.io.save_json_model(new_model, self.wd_log + "/../" + self.name + "_merged.json")
-    #     print("Nb of reactions in the merged model : ", len(new_model.reactions))
+        for reaction in self.pwt_reactions_id_list:
+            if reaction[0] in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']:
+                reaction = "_" + reaction
+            try:
+                added_reactions = copy.deepcopy(self.metacyc_model.reactions.get_by_id(reaction))
+                added_reactions_corrected = self._correct_pwt_gene_reaction_rule(added_reactions, verbose)
+                self.merged_model.add_reactions([added_reactions_corrected])
+            except KeyError:
+                pass
+        if trust_model:
+            for reaction in self.json_reactions_list:
+                self.merged_model.add_reactions([reaction])
+            for reaction in self.sbml_reactions_list:
+                self.merged_model.add_reactions([reaction])
+        # else:  # TODO
 
     def run(self):
         if utils.check_path(self.directory + "reactions.dat"):
             self._get_pwt_reactions()
             self._search_metacyc_reactions_ids()
-
         else:
-            print("No .dat files found, proceeding with drafts and models only.")
+            print("------\nNo .dat files found, proceeding with drafts and models only.\n------")
         if utils.check_path(self.directory, sys_exit=True):
             self._get_networks_reactions("json")
             self._get_networks_reactions("sbml")
+        self._merge()
+        cobra.io.save_json_model(self.merged_model, self.directory + self.name + "_merged.json")
+
+    # TODO : Check for PHOSCHOL-RXN if specificity is kept during merging process (cucumis_sativus)
 
 
 if __name__ == '__main__':
@@ -199,4 +199,3 @@ if __name__ == '__main__':
     # test = Merging("actinidia_chinensis", main_directory)
     # test = Merging("cucumis_sativus", main_directory)
     test.run()
-    print(len(set(test.json_reactions_list)))
